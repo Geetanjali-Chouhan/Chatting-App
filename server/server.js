0cc9c9ceaab9e28e3aa503c9866a9f357ea05d15
geetanjalichouhan <geetanjalichouhan82@gmail.com>
2026-07-30 22:19:15 +0530

    client mai ,env file add ki h

--- server/server.js
+++ server/server.js
@@ -1,23 +1,56 @@
-import dotenv from 'dotenv';
-dotenv.config();
-import express from 'express';
-import { connectDB } from './db/connection1.db.js';
+// import dotenv from 'dotenv';
+// dotenv.config();
+// import express from 'express';
+// import { connectDB } from './db/connection1.db.js';
+// connectDB();
+// const app = express();
+
+// app.use(express.json());
+
+// const PORT = process.env.PORT|| 5000;
+// // console.log(PORT)
+
+// // routes
+// import userRoute from './routes/user.route.js'
+// app.use('/api/v1/user',userRoute) 
+
+// // middleware
+// import { errorMiddleware } from './middlewares/error.middleware.js';
+// app.use(errorMiddleware); 
+
+// app.listen(PORT, () => {
+//   console.log(`Your server listening on port ${PORT}`)
+// })
+
+import { app, server } from "./socket/socket.js";
+import express from "express";
+import { connectDB } from "./db/connection1.db.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
 connectDB();
-const app = express();
 
+app.use(
+  cors({
+    origin: [process.env.CLIENT_URL],
+    credentials: true,
+  })
+);
 app.use(express.json());
+app.use(cookieParser());
 
-const PORT = process.env.PORT|| 5000;
-// console.log(PORT)
+const PORT = process.env.PORT || 5000;
 
 // routes
-import userRoute from './routes/user.route.js'
-app.use('/api/v1/user',userRoute) 
+import userRoute from "./routes/user.route.js";
+import messageRoute from "./routes/message.route.js";
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/message", messageRoute);
 
-// middleware
-import { errorMiddleware } from './middlewares/error.middleware.js';
-app.use(errorMiddleware); 
+// middlwares
+import { errorMiddleware } from "./middlewares/error.middlware.js";
+app.use(errorMiddleware);
 
-app.listen(PORT, () => {
-  console.log(`Your server listening on port ${PORT}`)
-})
+server.listen(PORT, () => {
+  console.log(`your server listening at port ${PORT}`);
+});
